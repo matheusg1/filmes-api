25d93f40f81d56e397880c5bb472d3ed6f7a0bd2
Matheus Gomes da Silva <matheus.silva@telecall.com>
2022-10-21 13:59:06 -0300

    Implementado POST e Annotations na classe Filme

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController
+    {
+        private static List<Filme> Filmes = new();
+        
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            Filmes.Add(filme);
+            System.Console.WriteLine($"Filme {filme.Titulo}");
+        }
+    }
+}
